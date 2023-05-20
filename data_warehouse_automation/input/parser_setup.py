672ca6a0e3811cf859a13390cd1ccc985b85badb
Amir Jaber <38318312+Terroface@users.noreply.github.com>
2023-05-20 20:01:02 +0200

    add profile reading

--- data_warehouse_automation/input/parser_setup.py
+++ data_warehouse_automation/input/parser_setup.py
@@ -6,36 +6,39 @@ upstrem modules accordingly.
 import argparse
 import os.path
 from data_warehouse_automation.cube.cube_name_tbc import generate_cube
-from data_warehouse_automation.input.project_values import read_project_file
+from data_warehouse_automation.input.configuration_values import read_project_file, read_profile_file
 
 
 def main():
     # Create the parser
-    parser = argparse.ArgumentParser(description='A simple argparse program')
+    parser = argparse.ArgumentParser(description='A simple argparse program' )
     
     # Create the path to the default configuration files
-    default_profile_dir = os.path.expanduser('~/.droughty/profile.yaml')
-    default_project_dir = os.path.join(os.getcwd(), 'dwa_project.yml')
+    default_profile_dir = os.path.expanduser( '~/.droughty/profile.yaml' )
+    default_project_dir = os.path.join( os.getcwd(), 'dwa_project.yml' )
 
     # Add subparsers for different commands
-    subparsers = parser.add_subparsers(dest='command', help='Available commands')
+    subparsers = parser.add_subparsers( dest='command', help='Available commands' )
 
     # Subparser for 'cube' command
-    test_parser = subparsers.add_parser('cube', help='Generates base cube syntax')
-    test_parser.add_argument('profile_dir', type=str, nargs='?', default=default_profile_dir, help='This command takes the path/to/your/profile.yml as input')
-    test_parser.add_argument('project_dir', type=str, nargs='?', default=default_project_dir, help='This command takes the path/to/your/project.yml as input')
+    test_parser = subparsers.add_parser( 'cube', help='Generates base cube syntax' )
+    test_parser.add_argument( 'profile_dir', type=str, nargs='?', default=default_profile_dir, help='This command takes the path/to/your/profile.yml as input' )
+    test_parser.add_argument( 'project_dir', type=str, nargs='?', default=default_project_dir, help='This command takes the path/to/your/project.yml as input' )
 
     # Parse the arguments
     args = parser.parse_args()
 
-    # Get the project configuration file information
-    profile_content = read_project_file( args.project_dir )
+    # Read the project configuration file
+    project_content = read_project_file( args.project_dir )
+    profile_content = read_profile_file( args.profile_dir, project_content['profile'] )
+
+    # Read the profile configuration file
 
     # Process based on the command
     if args.command == 'cube':
-        generate_cube(args.profile_dir, args.project_dir)
+        generate_cube( args.profile_dir, args.project_dir )
     else:
-        print('Invalid command')
+        print( 'Invalid command' )
 
 if __name__ == "__main__":
     main()
