0368dc56e2a2e7dbb5dcb882730a10261f13b7db
Amir Jaber <38318312+Terroface@users.noreply.github.com>
2023-05-20 19:13:34 +0200

    add project_dir input to parser

--- data_warehouse_automation/cube/cube_name_tbc.py
+++ data_warehouse_automation/cube/cube_name_tbc.py
@@ -4,5 +4,6 @@ This module TODO
 
 """
 
-def generate_cube(profile_dir):
-    print(f'Generating cube syntax for: {profile_dir}')
+
+def generate_cube(profile_dir, project_dir):
+    print(f'Generating cube syntax for profile {profile_dir} and project {project_dir}')
--- data_warehouse_automation/input/parser_setup.py
+++ data_warehouse_automation/input/parser_setup.py
@@ -6,21 +6,23 @@ upstrem modules accordingly.
 import argparse
 import os.path
 from data_warehouse_automation.cube.cube_name_tbc import generate_cube
-# from data_warehouse_automation.input.config_file_locations import ?
+
 
 def main():
     # Create the parser
     parser = argparse.ArgumentParser(description='A simple argparse program')
     
-    # Create the path to the default profile
+    # Create the path to the default configuration files
     default_profile_dir = os.path.expanduser('~/.droughty/profile.yaml')
+    default_project_dir = os.path.join(os.getcwd(), 'dwa_project.yml')
 
     # Add subparsers for different commands
     subparsers = parser.add_subparsers(dest='command', help='Available commands')
 
     # Subparser for 'cube' command
     test_parser = subparsers.add_parser('cube', help='Generates base cube syntax')
-    test_parser.add_argument('profile_dir', type=str, nargs='?', default=default_profile_dir, help='This command takes the path/to/yourprofile.yml as input')
+    test_parser.add_argument('profile_dir', type=str, nargs='?', default=default_profile_dir, help='This command takes the path/to/your/profile.yml as input')
+    test_parser.add_argument('project_dir', type=str, nargs='?', default=default_project_dir, help='This command takes the path/to/your/project.yml as input')
 
 
     # Parse the arguments
@@ -28,7 +30,7 @@ def main():
 
     # Process based on the command
     if args.command == 'cube':
-        generate_cube(args.profile_dir)
+        generate_cube(args.profile_dir, args.project_dir)
     else:
         print('Invalid command')
 
