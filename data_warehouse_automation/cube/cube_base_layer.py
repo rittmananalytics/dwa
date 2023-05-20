8c4081e698195118e048e5dda8a703502c1948f8
Amir Jaber <38318312+Terroface@users.noreply.github.com>
2023-05-20 23:46:55 +0200

    beautified

--- data_warehouse_automation/cube/cube_base_layer.py
+++ data_warehouse_automation/cube/cube_base_layer.py
@@ -4,9 +4,18 @@ This module generates the cube base layer
 
 """
 
+# Convert a string to camel case
 def to_camel_case(name):
     components = name.split('_')
     return components[0].lower() + ''.join(x.title() for x in components[1:])
+
+# Function to add new lines and indentation to a dictionary string
+def format_dict_string(dict_string):
+    parts = dict_string.split('{ ', 1)  # Split at the first squiggkly bracket after a dimension or measure name
+    parts[1] = parts[1].rstrip('}') # remove closing squiggly bracket of each dimension or measure
+    parts[1] = parts[1].replace(', ', ',\n      ')  # Replace every comma in a dimension or measure with a comma followed by a newline
+    formatted_parts = [parts[0] + '{\n      ' + parts[1] + '\n    }']
+    return ''.join(formatted_parts)
 
 def generate_cube_js_base_file(tables_columns, file_path):
 
@@ -45,15 +54,21 @@ def generate_cube_js_base_file(tables_columns, file_path):
 
             # Write dimensions
             file.write('  dimensions: {\n\n')
-            file.write(',\n\n'.join(dimensions))
+            print(dimensions)
+            formatted_dimensions = [format_dict_string(dim) for dim in dimensions]
+            file.write(',\n\n'.join('    ' + dim for dim in formatted_dimensions))
+            # for dim in formatted_dimensions:
+            #     print(format_dict_string(dim))
+            #     print(dim)
             file.write('\n\n  },\n\n')
 
             # Write measures
             file.write('  measures: {\n\n')
-            file.write(',\n\n'.join(measures))
+            formatted_measures = [format_dict_string(measure) for measure in measures]
+            file.write(',\n\n'.join('    ' + measure for measure in formatted_measures))
             if measures:
                 file.write(',\n\n')
-            file.write(f'    count{table_name_camel_case.capitalize()}: {{ type: "count" }}\n\n  }}\n')
+            file.write(f'    count{table_name_camel_case.capitalize()}: {{\n      type: "count"\n    }}\n\n  }}\n')
 
             # Write the end of the cube
             file.write('});\n\n')
