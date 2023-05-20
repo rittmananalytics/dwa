db3a1604b038a2f1f614c410fa3831c56f5c7973
Amir Jaber <38318312+Terroface@users.noreply.github.com>
2023-05-20 22:52:11 +0200

    cube base might work but looks bad

--- data_warehouse_automation/cube/cube_base_layer.py
+++ data_warehouse_automation/cube/cube_base_layer.py
@@ -4,18 +4,26 @@ This module generates the cube base layer
 
 """
 
-
-# def generate_cube(profile_dir, project_dir):
-#     print(f'Generating cube syntax for profile {profile_dir} and project {project_dir}')
+def to_camel_case(name):
+    components = name.split('_')
+    return components[0].lower() + ''.join(x.title() for x in components[1:])
 
 def generate_cube_js_base_file(tables_columns, file_path):
 
     # Open the file for writing
     with open(file_path, 'w') as file:
+        # Write the import statement at the top of the file
+        file.write('import { databaseSchema, databaseName } from \'../tablePrefix\';\n\n')
+
         # Iterate over each table in the dictionary
         for table_name, columns in tables_columns.items():
             # Write the cube definition for the table
-            file.write(f'cube(`{table_name}`) {{\n')
+            table_name_camel_case = to_camel_case(table_name)
+            file.write(f'cube(`{table_name_camel_case}`, {{\n\n')
+
+            file.write(f'  sql: `SELECT * FROM ${{databaseName()}}.${{databaseSchema()}}."{table_name}"`,\n\n')
+
+            file.write('  shown: false,\n\n')
 
             # Initialize dimensions and measures
             dimensions = []
@@ -25,23 +33,27 @@ def generate_cube_js_base_file(tables_columns, file_path):
             for column_info in columns:
                 column_name = column_info['column_name']
                 data_type = column_info['data_type']
+                column_name_camel_case = to_camel_case(column_name)
 
                 # Check if the column is a primary key dimension
-                if column_name.endswith('_pk') or 'text' in data_type.lower():
-                    dimensions.append(f'    `{column_name}`: {{ sql: `{column_name}`, type: "string" }}')
+                if column_name.lower().endswith('_pk'):
+                    dimensions.append(f'{column_name_camel_case}: {{ sql: `${{CUBE}}."{column_name}"`, type: "string", primaryKey: true }}')
+                elif 'text' in data_type.lower():
+                    dimensions.append(f'{column_name_camel_case}: {{ sql: `${{CUBE}}."{column_name}"`, type: "string" }}')
                 elif 'number' in data_type.lower():
-                    measures.append(f'    `{column_name}`: {{ sql: `{column_name}`, type: "sum" }}')
+                    measures.append(f'sum{column_name_camel_case.capitalize()}: {{ sql: `${{CUBE}}."{column_name}"`, type: "sum" }}')
 
             # Write dimensions
-            file.write('  dimensions: {\n')
-            file.write(',\n'.join(dimensions))
-            file.write('\n  },\n')
+            file.write('  dimensions: {\n\n')
+            file.write(',\n\n'.join(dimensions))
+            file.write('\n\n  },\n\n')
 
             # Write measures
-            file.write('  measures: {\n')
-            file.write(',\n'.join(measures))
+            file.write('  measures: {\n\n')
+            file.write(',\n\n'.join(measures))
             if measures:
-                file.write(',\n')
-            file.write('    count: { type: "count" }\n  }\n')
+                file.write(',\n\n')
+            file.write(f'    count{table_name_camel_case.capitalize()}: {{ type: "count" }}\n\n  }}\n')
 
-            file.write('}\n\n')
+            # Write the end of the cube
+            file.write('});\n\n')
