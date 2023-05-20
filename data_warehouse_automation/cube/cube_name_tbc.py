bfb3d8db3198c1ad223c8d8c1194794a99995ba9
Amir Jaber <38318312+Terroface@users.noreply.github.com>
2023-05-20 18:17:21 +0200

    poc: importing from other modules

---
+++ data_warehouse_automation/cube/cube_name_tbc.py
@@ -0,0 +1,7 @@
+
+"""
+This module TODO
+"""
+
+def generate_cube(profile_dir):
+    print(f'Generating cube syntax for: {profile_dir}')
--- data_warehouse_automation/parser/parser_setup.py
+++ data_warehouse_automation/parser/parser_setup.py
@@ -1,4 +1,9 @@
+"""
+This module defines the available commands, their input parameters, and kicks off relevant
+upstrem modules accordingly.
+"""
 import argparse
+from data_warehouse_automation.cube.cube_name_tbc import generate_cube
 
 def main():
     # Create the parser
@@ -7,39 +12,19 @@ def main():
     # Add subparsers for different commands
     subparsers = parser.add_subparsers(dest='command', help='Available commands')
 
-    # Subparser for 'input' command
-    input_parser = subparsers.add_parser('input', help='Process input')
-    input_parser.add_argument('path', type=str, help='Input file path')
+    # Subparser for 'cube' command
+    test_parser = subparsers.add_parser('cube', help='Generates base cube syntax')
+    test_parser.add_argument('profile_dir', type=str, help='This command takes the path/to/yourprofile.yml as input')
 
-    # Subparser for 'test' command
-    test_parser = subparsers.add_parser('test', help='Run tests')
-    test_parser.add_argument('example', type=str, help='Test example')
-
-    # Subparser for 'build' command
-    build_parser = subparsers.add_parser('build', help='Build something')
-    build_parser.add_argument('example', type=str, help='Build example')
 
     # Parse the arguments
     args = parser.parse_args()
 
     # Process based on the command
-    if args.command == 'input':
-        process_input(args.path)
-    elif args.command == 'test':
-        run_tests(args.example)
-    elif args.command == 'build':
-        build_something(args.example)
+    if args.command == 'cube':
+        generate_cube(args.profile_dir)
     else:
         print('Invalid command')
-
-def process_input(path):
-    print(f'Processing input: {path}')
-
-def run_tests(example):
-    print(f'Running tests for: {example}')
-
-def build_something(example):
-    print(f'Building: {example}')
 
 if __name__ == "__main__":
     main()
