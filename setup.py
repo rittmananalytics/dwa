007a2b8e8669c849042ed460dc7c315e1cc256fe
Amir Jaber <38318312+Terroface@users.noreply.github.com>
2023-05-22 11:26:52 +0200

    updated setup.py version to 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='data_warehouse_automation',
-    version='0.1',
+    version='0.3.0',
     packages=find_packages(),
     install_requires=[
         'pyyaml',
